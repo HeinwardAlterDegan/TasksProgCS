3faa7ef5c37f146d3b3f82a0041cc33ab1d8dc8f
Эдуард Гусев <f1ful1337@gmail.com>
2024-10-27 20:36:46 +0500

    Вариант №7. Ln(x) при х>1 -Ln(x) при 0<x<=1 0 при x<=0Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksProgCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение X");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("f(x)=" + CalculateFunction(x));
+        }
+        static double CalculateFunction(double x)
+        {
+            if (x > 1) { return Math.Log(x); }
+
+            else if (x > 0 && x <= 1)
+            { return -Math.Log(x); }
+            else { return 0; }; 
+
+
+        }
+    }
+} 
