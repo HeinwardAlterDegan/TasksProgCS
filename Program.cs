0a915fd153a22218a761de235acaf92791026b98
Эдуард Гусев <f1ful1337@gmail.com>
2024-10-27 21:31:38 +0500

    Вариант№7

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace TasksProgCs
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Дано выражение:x= (2+e^(-2))/(3) + ((3+e^(-3))/(5) + (5+5^(-5))/7)");
+            Console.WriteLine("");
+            Console.WriteLine("X="+(CalculateX(3)+CalculateX(5)+CalculateX(7)));
+        }
+        static double CalculateX(double n)
+        {
+            return (1 / n) * ((Math.Round(n * 2 / 3) + Math.Pow(Math.E, -(n * 2 / 3))));
         }
     }
 }
