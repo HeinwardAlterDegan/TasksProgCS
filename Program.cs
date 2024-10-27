8e34d7765e4e923ab0471b5cc778371561d96485
Эдуард Гусев <f1ful1337@gmail.com>
2024-10-27 20:49:32 +0500

    Вариант 7 Дано выражение: y=|x+|x+|x+√(x+1)|||

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace TasksProgCs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Дано выражение: y=|x+|x+|x+√(x+1)|||");
+            Console.WriteLine("");
+            Console.WriteLine("Введите значение x, чтобы получить y");
+            var x = double.Parse( Console.ReadLine());
+            Console.WriteLine("Значение y ="+CalculateY(x));
+        }
+        static double CalculateY(double x)
+        {
+            var y = Math.Abs(x + Math.Abs(x + Math.Abs(x + Math.Sqrt(x + 1))));
+            return y;
         }
     }
 }
