18c162e157f92a7f1ef17dc9fbb9cb8cdcc88d9a
Эдуард Гусев <f1ful1337@gmail.com>
2025-01-02 18:32:48 +0500

    Задание 7.1 Готово

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,25 @@ namespace TasksProgCs
         static void Main(string[] args)
         {
             Console.WriteLine("Введите k,m,n");
-            int k = Console.ReadKey();
-            int m = Console.ReadKey();
-            int n = int.Parse(Console.ReadKey());
+            var k = int.Parse(Console.ReadLine()); 
+            var m = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+
+            if (IsDevisibleByThree(k, m, n)) 
+            {
+                Console.WriteLine("Числа кратны трём");
+            }
+            else { Console.WriteLine("Числа не кратны трём"); }
+        }
+        static bool IsDevisibleByThree (int k, int m, int n)
+        {
+            if ( (k % 3 == 0) && (m % 3 == 0) && (n % 3 == 0))
+            {
+                return true;
+            } 
+            return false;
+           
+           
         }
     }
 }
