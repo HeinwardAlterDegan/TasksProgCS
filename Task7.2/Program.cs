87c51b62e9afaabf8bb916e4f89b82ca22784821
Эдуард Гусев <f1ful1337@gmail.com>
2025-01-04 14:03:54 +0500

    Сделано задание 7.2

---
+++ Task7.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки на плоскости:");
+            Console.WriteLine("Координата х:");
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Координата у:");
+            var y = int.Parse(Console.ReadLine());
+            if (IsDotInTheArea(x , y))
+            {
+                Console.WriteLine("Точка лежит в области");
+            }
+            else { Console.WriteLine("Точка не лежит в области"); }
+            Console.ReadKey();
+        }
+        static bool IsDotInTheArea(int x, int y) 
+        {
+            if ((x <= -3 && x >= -2) || (x >=-1 && y >= -2) ) { return true; }
+            else return false;
+        }
+    }
+}
