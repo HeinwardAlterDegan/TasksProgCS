e8d73b1af3d52481398d5abca1bcb1b889324f78
Эдуард Гусев <f1ful1337@gmail.com>
2025-01-08 19:34:17 +0500

    Выполнено задание 10.1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,42 @@ namespace TasksProgCs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число целое а. Оно должно быть меньше 500");
+            int a;
+
+            if (!TryInputNumber(out a))
+            {
+                Console.ReadKey();
+                return;
+            }
+            if (a >= 500)
+            {
+                Console.WriteLine("Число а больше или равно 500");
+                Console.ReadKey();
+                return;
+            }
+
+            double product = 0;
+
+            for (double i = a; i < 500; i++)
+            {
+                product += Math.Pow(i, 3);
+            }
+
+            Console.WriteLine($"Сумма кубов чисел от a до 500 ={product} ");
+        }
+
+        static bool TryInputNumber(out int number)
+        {
+            number = 0;
+            if (!int.TryParse(Console.ReadLine(), out int n))
+            {
+                Console.WriteLine("Введено неверное число.");
+                return false;
+            }
+
+            number = n;
+            return true;
         }
     }
 }
